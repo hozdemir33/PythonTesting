df6f927902c89b86e407af5869c035370f80bb98
hozdemir33 <hozdemir8233@gmail.com>
2025-02-06 21:38:05 -0800

    first commit

---
+++ hello.py
@@ -0,0 +1,11 @@
+#print("hello")
+
+def first_class():
+    print("this is the first function..")
+
+def test_test():
+    first_class()
+
+
+def test_second_test():
+    print("this is the second test....")
---
+++ mathfunction.py
@@ -0,0 +1,8 @@
+def test_add():
+    print(20+30)
+
+
+def test_subtruct():
+    print(20-1)
+
+
